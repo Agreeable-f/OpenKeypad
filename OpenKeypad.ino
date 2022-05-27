3404b0dad74d8ffb6dd344fdde761ce536ae9221
Agreeable <fsalatti2@outlook.com>
2022-05-27 17:01:46 -0400

    moving in sunny edit
    moves in sunny's edit of simplifying button definitions

--- OpenKeypad.ino
+++ OpenKeypad.ino
@@ -1,30 +1,8 @@
 
 //Original Code from here(https://bit.ly/3vGVAlB)
 
-// Button definitions
-#define BUTTON_KEY1 's'                                        //assign q/e
-//#define BUTTON_KEY2    
-#define BUTTON_KEY3 'd'                                     //assign e/q
-//#define BUTTON_KEY4                            
-//#define BUTTON_KEY5 
-//#define BUTTON_KEY6 
-//#define BUTTON_KEY7 
-//#define BUTTON_KEY8 
-//#define BUTTON_KEY9 
- 
-// Pin definitions
-#define BUTTON_PIN1 5                            
-//#define BUTTON_PIN2 16
-#define BUTTON_PIN3 15                              
-//#define BUTTON_PIN4 5
-//#define BUTTON_PIN5 6
-//#define BUTTON_PIN6 7
-//#define BUTTON_PIN7 8
-//#define BUTTON_PIN8 9
-//#define BUTTON_PIN9 10
-//#define BUTTON_PIN10 
-// ---------------------------------
- 
+//Thanks Sunny for the edits :D
+
 #include "Keyboard.h"
  
 // Button helper class for handling press/release and debouncing
@@ -56,24 +34,35 @@ class button {
   boolean pressed = 0;
 } ;
  
-// Button objects, organized in array
+// ---------------------------------
+
+// Add buttons to this array, in the form { key, pin }
 button buttons[] = {
-  {BUTTON_KEY1, BUTTON_PIN1},
-  {BUTTON_KEY3, BUTTON_PIN3},
-  //{BUTTON_KEY3, BUTTON_PIN3},
-  //{BUTTON_KEY4, BUTTON_PIN4},
-  //{BUTTON_KEY5, BUTTON_PIN5},
-  //{BUTTON_KEY6, BUTTON_PIN6},
-  //{BUTTON_KEY7, BUTTON_PIN7},
-  //{BUTTON_KEY8, BUTTON_PIN8},
-  //{BUTTON_KEY9, BUTTON_PIN9},
+  //{' ', 1},
+  //{' ', 2},
+  //{' ', 3},
+  //{' ', 4},
+  {'z', 5},
+  //{' ', 6},
+  //{' ', 7},
+  //{' ', 8},
+  //{' ', 9},
+  //{' ', 10},
+  //{' ', 11},
+  //{' ', 12},
+  //{' ', 13},
+  //{' ', 14},
+  {'x', 15},
+  //{' ', 16},
 };
  
 const uint8_t NumButtons = sizeof(buttons) / sizeof(button);
 const uint8_t ledPin = 17;
 
+// ---------------------------------
+
 void setup() {
-    // Safety check. Ground pin #1 (RX) to cancel keyboard inputs.
+   // Safety check. Ground pin #1 (RX) to cancel keyboard inputs.
   pinMode(1, INPUT_PULLUP);
   if(!digitalRead(1)){
     failsafe();
